283329fe19ac2a115b1f00fdf84223c839a89ec0
jqkamal <ramkamal5886@gmail.com>
2025-01-13 12:56:13 +0530

    new

--- forms/contact.js
+++ forms/contact.js
@@ -1,26 +1,2 @@
-fetch('forms/contact.js')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.text();
-  })
-  .then(data => {
-    console.log(data); // Do something with the JS file content
-  })
-  .catch(error => {
-    console.error('Fetch error:', error);
-  });
 
-  document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-  
-    emailjs.sendForm('your_service_id', 'your_template_id', this)
-      .then(() => {
-        alert('Message sent successfully!');
-      })
-      .catch((error) => {
-        alert('Failed to send message:', error);
-      });
-  });
   
